423347cc472de291b68a9d19a7260569e4c15cf7
Iago Gabriel Nobre de Macedo <iagognobre@gmail.com>
2025-12-05 21:02:40 -0300

    feature: adding spring project

---
+++ smartparking/src/test/java/br/ufrn/imd/smartparking/smartparking/SmartParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.smartparking.smartparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
